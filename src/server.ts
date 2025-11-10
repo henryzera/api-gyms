387201183ccbe7a230f621ed8fc1e43ad1377be5
Emanuel Henry <emanuelhenrysc@gmail.com>
2025-11-10 17:11:01 -0300

    chore: prisma configs

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import { app } from "./app.js";
-import { env } from "./env/index.js";
+import { env } from "@/env/index.js";
 
 app.listen({
     host: '0.0.0.0',
     port: env.PORT,
 }).then(()=>{
     console.log('🚀 HTTP Server running')
+    console.log(process.env.DATABASE_URL)
 })
