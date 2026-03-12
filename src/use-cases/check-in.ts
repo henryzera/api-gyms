34feb933f3f9c93a63d87c97239218c86bf49fdc
Emanuel Henry <emanuelhenrysc@gmail.com>
2026-03-12 18:57:54 -0300

    feat: implement distance calculation for check-in validation

--- src/use-cases/check-in.ts
+++ src/use-cases/check-in.ts
@@ -2,6 +2,7 @@ import type { GymsRepository } from '@/repositories/gyms-repository.js'
 import type { CheckInsRepository } from '../repositories/check-ins-repository.js'
 import type { CheckIn } from '@prisma/client'
 import { ResourceNotFoundError } from './errors/resource-not-found-error.js'
+import { getDistanceBetweenCoordinates } from '@/utils/get-distance-between-cordinates.js'
 
 interface CheckInUseCaseRequest {
   userId: string
@@ -29,6 +30,8 @@ export class CheckInUseCase {
   async execute({
     userId,
     gymId,
+    userLatitude,
+    userLongitude,
   }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
     const gym = await this.gymsRepository.findById(gymId)
 
@@ -37,6 +40,23 @@ export class CheckInUseCase {
     }
 
     // calcular distancia entre user e gym
+
+    const distance = getDistanceBetweenCoordinates(
+      {
+        latitude: userLatitude,
+        longitude: userLongitude,
+      },
+      {
+        latitude: gym.latitude.toNumber(),
+        longitude: gym.longitude.toNumber(),
+      },
+    )
+
+    const MAX_DISTANCE_IN_KILOMETERS = 0.1
+
+    if (distance > MAX_DISTANCE_IN_KILOMETERS) {
+      throw new Error()
+    }
 
     const checkinOnSameDate = await this.checkInsRepository.findByUserIdOnDate(
       userId,
