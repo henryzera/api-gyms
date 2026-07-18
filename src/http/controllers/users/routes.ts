bb2ad00e36b20054e3805ea181ffe60fef7067fa
Emanuel Henry <emanuelhenrysc@gmail.com>
2026-07-18 00:44:24 -0300

    feat: gyms creation controller

---
+++ src/http/controllers/users/routes.ts
@@ -0,0 +1,14 @@
+import type { FastifyInstance } from 'fastify'
+import { register } from './register.js'
+import { authenticate } from './authenticate.js'
+import { profile } from './profile.js'
+import { verifyJWT } from '../../middlewares/verify-jwt.js'
+
+export async function usersRoutes(app: FastifyInstance) {
+  /** Public */
+  app.post('/users', register)
+  app.post('/sessions', authenticate)
+
+  /** Authenticated */
+  app.get('/me', { onRequest: [verifyJWT] }, profile)
+}
